fa7565f7e57ee95fa19b1a53e27079b5b1d359be
william quimbay <quimbayfeo@gmail.com>
2025-11-06 00:22:59 -0500

    commit

---
+++ app/src/main/java/com/odinsystem/tiendavirtual/SplashScreenActivity.kt
@@ -0,0 +1,32 @@
+package com.odinsystem.tiendavirtual
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.CountDownTimer
+import androidx.appcompat.app.AppCompatActivity
+
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        verBienvenida()
+
+    }
+    private fun verBienvenida() {
+        object : CountDownTimer(3000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+
+            }
+
+            override fun onFinish() {
+                startActivity(Intent(this@SplashScreenActivity, MainActivity::class.java))
+                finishAffinity()
+            }
+
+        }.start()
+
+    }
+}
---
+++ app/src/main/java/com/odinsystem/tiendavirtual/Vendedor/MainActivityVendedor.kt
@@ -0,0 +1,17 @@
+package com.odinsystem.tiendavirtual.Vendedor
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.odinsystem.tiendavirtual.R
+
+class MainActivityVendedor : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main_vendedor)
+
+    }
+}
